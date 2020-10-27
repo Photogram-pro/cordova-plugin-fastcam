8c4fae0f6598a657d6efb4a111d062509dfc334d
Maximilian Torggler <maximilian.torggler@tuta.io>
2020-10-27 14:19:47 +0100

    feat: optional GPS device configuration

---
+++ src/android/GpsCommunication.java
@@ -0,0 +1,142 @@
+package com.cordovapluginfastcam;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+/**
+ * Communication with GPS
+ * device via USB serial.
+ * It's a singleton,
+ * as only one instance
+ * should exist per app
+ */
+public class GpsCommunication implements SerialInputOutputManager.Listener {
+    private static GpsCommunication instance = null;
+    private static final String TAG = "GpsCommunication";
+    private static final String USB_PERMISSION = "com.photogram.cmaeratestapp.USB_PERMISSION";
+    private UsbSerialPort port;
+    private SerialInputOutputManager usbIoManager;
+    private NMEA nmeaParser = new NMEA();
+    private NMEA.GPSPosition currentPosition;
+    private int baudRate = 115200;
+    private GpsDataCallback callback;
+    private BroadcastReceiver broadcastReceiver;
+
+    private Activity activity;
+
+
+    public GpsCommunication(Activity activity) {
+        this.activity = activity;
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().equals(USB_PERMISSION)) {
+                    initialize();
+                }
+            }
+        };
+        activity.registerReceiver(broadcastReceiver, new IntentFilter(USB_PERMISSION));
+    }
+
+    public static GpsCommunication getInstance(Activity activity) {
+        if (GpsCommunication.instance == null) {
+            GpsCommunication.instance = new GpsCommunication(activity);
+        }
+        return GpsCommunication.instance;
+    }
+
+    public static GpsCommunication getInstance() {
+        return GpsCommunication.instance;
+    }
+
+    public void configure(int baudRate) {
+        if (baudRate == 0) {
+            return;
+        }
+        this.baudRate = baudRate;
+    }
+
+    public void initialize() {
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) this.activity.getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            Log.d(TAG, "No USB devices found!");
+            return;
+        }
+
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            Log.d(TAG, "Need to request permission!");
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this.activity, 0, new Intent(this.USB_PERMISSION), 0);
+            // and a filter on the permission we ask
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(this.USB_PERMISSION);
+            manager.requestPermission(driver.getDevice(), pendingIntent);
+            Log.d(TAG, "Permission requested.");
+            return;
+        }
+
+         port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        try {
+            port.open(connection);
+            port.setParameters(this.baudRate, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        usbIoManager = new SerialInputOutputManager(port, this);
+        Executors.newSingleThreadExecutor().submit(usbIoManager);
+    }
+
+    @Override
+    public void onNewData(byte[] bytes) {
+        String strData = new String(bytes, StandardCharsets.UTF_8);
+        this.currentPosition = nmeaParser.parse(strData, "GGA");
+        if (this.callback != null) {
+            this.callback.onData(this.currentPosition);
+        }
+    }
+
+    @Override
+    public void onRunError(Exception e) {
+        e.printStackTrace();
+    }
+
+    public void close() {
+        if (port != null) {
+            try {
+                port.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public NMEA.GPSPosition getCurrentPosition() {
+        return this.currentPosition;
+    }
+
+    public void setDataCallback(GpsDataCallback cb) {
+        this.callback = cb;
+    }
+}
---
+++ src/android/NMEA.java
@@ -0,0 +1,167 @@
+package com.cordovapluginfastcam;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * From: https://gist.github.com/javisantana/1326141/30d6b5b603fa113d7a17bfcc0a8aaa25a107d581
+ */
+public class NMEA {
+
+    interface SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position);
+    }
+
+    // utils
+    static float Latitude2Decimal(String lat, String NS) {
+        if (lat.length() < 2) return 0f;
+        float med = Float.parseFloat(lat.substring(2))/60.0f;
+        med +=  Float.parseFloat(lat.substring(0, 2));
+        if(NS.startsWith("S")) {
+            med = -med;
+        }
+        return med;
+    }
+
+    static float Longitude2Decimal(String lon, String WE) {
+        if (lon.length() < 3) return 0f;
+        float med = Float.parseFloat(lon.substring(3))/60.0f;
+        med +=  Float.parseFloat(lon.substring(0, 3));
+        if(WE.startsWith("W")) {
+            med = -med;
+        }
+        return med;
+    }
+
+    // parsers
+    class GPGGA implements SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position) {
+            position.time = Float.parseFloat(tokens[1]);
+            position.lat = Latitude2Decimal(tokens[2], tokens[3]);
+            position.lon = Longitude2Decimal(tokens[4], tokens[5]);
+            position.quality = Integer.parseInt(tokens[6]);
+            position.altitude = Float.parseFloat(tokens[9]);
+            return true;
+        }
+    }
+
+    class GPGGL implements SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position) {
+            position.lat = Latitude2Decimal(tokens[1], tokens[2]);
+            position.lon = Longitude2Decimal(tokens[3], tokens[4]);
+            position.time = Float.parseFloat(tokens[5]);
+            return true;
+        }
+    }
+
+    class GPRMC implements SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position) {
+            position.time = Float.parseFloat(tokens[1]);
+            position.lat = Latitude2Decimal(tokens[3], tokens[4]);
+            position.lon = Longitude2Decimal(tokens[5], tokens[6]);
+            position.velocity = Float.parseFloat(tokens[7]);
+            position.dir = Float.parseFloat(tokens[8]);
+            return true;
+        }
+    }
+
+    class GPVTG implements SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position) {
+            position.dir = Float.parseFloat(tokens[3]);
+            return true;
+        }
+    }
+
+    class GPRMZ implements SentenceParser {
+        public boolean parse(String [] tokens, GPSPosition position) {
+            position.altitude = Float.parseFloat(tokens[1]);
+            return true;
+        }
+    }
+
+    public class GPSPosition {
+        public float time = 0.0f;
+        public float lat = 0.0f;
+        public float lon = 0.0f;
+        public boolean fixed = false;
+        public int quality = 0;
+        public float dir = 0.0f;
+        public float altitude = 0.0f;
+        public float velocity = 0.0f;
+
+        public void updatefix() {
+            fixed = quality > 0;
+        }
+
+        public String toString() {
+            return String.format("POSITION: lat: %f, lon: %f, time: %f, Q: %d, dir: %f, alt: %f, vel: %f", lat, lon, time, quality, dir, altitude, velocity);
+        }
+
+        public JSONObject toJson() {
+            JSONObject json = new JSONObject();
+            try {
+                json.put("altitude", this.altitude);
+                json.put("dir",  this.dir);
+                json.put("fixed", this.fixed);
+                json.put("lat", this.lat);
+                json.put("lon", this.lon);
+                json.put("quality", this.quality);
+                json.put("time", this.time);
+                json.put("velocity", this.velocity);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return json;
+        }
+    }
+
+    GPSPosition position = new GPSPosition();
+
+    private static final Map<String, SentenceParser> sentenceParsers = new HashMap<String, SentenceParser>();
+
+    public NMEA() {
+        sentenceParsers.put("GGA", new GPGGA());
+        sentenceParsers.put("GGL", new GPGGL());
+        sentenceParsers.put("RMC", new GPRMC());
+        sentenceParsers.put("RMZ", new GPRMZ());
+        //only really good GPS devices have this sentence but ...
+        sentenceParsers.put("VTG", new GPVTG());
+    }
+
+    public GPSPosition parse(String lines, String typeFilter) {
+        String[] splitted = lines.split("\n");
+        for (int i = 0; i < splitted.length; i++) {
+            String line = splitted[i];
+            if(line.startsWith("$")) {
+                String nmea = line.substring(1);
+                String[] tokens = nmea.split(",");
+                if(tokens[0].length() < 2) {
+                    continue;
+                }
+                String type = tokens[0].substring(2);
+
+                if (!type.equals(typeFilter)) {
+                    continue;
+                }
+                //TODO check crc
+                try {
+                    if(sentenceParsers.containsKey(type)) {
+                        sentenceParsers.get(type).parse(tokens, position);
+                    }
+                    position.updatefix();
+                } catch (Exception e) {
+                    Log.d("NMEA", "Nmea parse error. Continue...");
+                }
+            }
+        }
+
+        return position;
+    }
+
+
+}
